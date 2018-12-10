6e06cbad773b8fa5a11b5559c2c2270205d54328
sensible-shell-8529 <diana.lee116@gmail.com>
2018-12-10 22:51:43 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,33 @@
+function shout(string){
+  return string.toUpperCase()
+}
+
+function whisper(string){
+return string.toLowerCase()
+}
+
+function logShout(string){
+  var spy = "hello";
+ console.log(spy.toUpperCase())
+}
+
+function logWhisper(string){
+  var spy = "HELLO";
+  console.log(spy.toLowerCase())
+}
+
+function sayHiToGrandma(string){
+  var  str="";
+  var one= "hello"
+  var two="HELLO"
+  var three="I love you,Grandma."
+  if (one.toLowerCase()=== one){
+    console.log(str = "I can't hear you!")
+  }else if (two.toUpperCase()=== two){
+    console.log(str = "YES INDEED!")
+  }
+    else if (three === three){
+      console.log(str = "I love you, too.")
+  }
+  return str
+}
